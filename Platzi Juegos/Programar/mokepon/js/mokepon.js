807a22b42f55b13ead7b9999c9823789b9cee46e
Krezitech-Win <krezipedtech@gmail.com>
2023-03-06 14:06:47 -0600

    Corrección errores aleatoriedad de ataques enemigos

--- Platzi Juegos/Programar/mokepon/js/mokepon.js
+++ Platzi Juegos/Programar/mokepon/js/mokepon.js
@@ -1,7 +1,7 @@
-let ataqueJugador 
+let ataqueJugador
 let ataqueEnemigo
 
-function iniciarJuego(){
+function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
@@ -9,103 +9,91 @@ function iniciarJuego(){
     botonFuego.addEventListener('click', ataqueFuego)
     let botonAgua = document.getElementById('boton-agua')
     botonAgua.addEventListener('click', ataqueAgua)
-    let botonTierra  = document.getElementById('boton-tierra')
+    let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
-
 }
 
 function seleccionarMascotaJugador() {
-
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
-    let inputLangostelvis = document.getElementById('langostelvis')
-    let inputTucapalma = document.getElementById('tucapalma')
-    let inputPydos = document.getElementById('pydos')
-
     let spanMascotaJugador = document.getElementById('mascota-jugador')
 
-    if (inputHipodoge.checked){
+    if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML = 'Hipodoge'
-    }
-    else if (inputCapipepo.checked){
+    } else if (inputCapipepo.checked) {
         spanMascotaJugador.innerHTML = 'Capipepo'
-    }
-    else if (inputRatigueya.checked){
+    } else if (inputRatigueya.checked) {
         spanMascotaJugador.innerHTML = 'Ratigueya'
-    }
-    else if (inputLangostelvis.checked){
-        spanMascotaJugador.innerHTML = 'Langostelvis'
-    }
-    else if (inputTucapalma.checked){
-        spanMascotaJugador.innerHTML = 'Tucapalma'
-    }
-    else if (inputPydos.checked){
-        spanMascotaJugador.innerHTML = 'Pydos'
-    }
-    else {
+    } else {
         alert('NO ELEGISTE NINGUNA MASCOTA')
     }
     seleccionarMascotaEnemigo()
 }
-
-function seleccionarMascotaEnemigo(){
-    let mascotaAleatorio = aleatorio(1,6)
+function seleccionarMascotaEnemigo() {
+    let mascotaAleatoria = aleatorio(1,3)
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
-    if(mascotaAleatorio == 1){
+    if(mascotaAleatoria == 1){
         spanMascotaEnemigo.innerHTML = 'Hipodoge'
-    } else if (mascotaAleatorio == 2) {
+    } else if (mascotaAleatoria == 2) {
         spanMascotaEnemigo.innerHTML = 'Capipepo'
-    } else if (mascotaAleatorio == 3) {
+    } else  {
         spanMascotaEnemigo.innerHTML = 'Ratigueya'
-    } else if (mascotaAleatorio == 4) {
-        spanMascotaEnemigo.innerHTML = 'Langostelvis'
-    } else if (mascotaAleatorio == 5) {
-        spanMascotaEnemigo.innerHTML = 'Tucapalma'
-    } else if(mascotaAleatorio == 6) {
-        spanMascotaEnemigo.innerHTML = 'Pydos'
     }
 }
-
-function ataqueFuego(){
+function ataqueFuego() {
     ataqueJugador = 'FUEGO'
     ataqueAleatorioEnemigo()
 }
-function ataqueAgua(){
+function ataqueAgua() {
     ataqueJugador = 'AGUA'
     ataqueAleatorioEnemigo()
 }
-function ataqueTierra(){
+function ataqueTierra() {
     ataqueJugador = 'TIERRA'
     ataqueAleatorioEnemigo()
 }
 
-function ataqueAleatorioEnemigo(){
+function ataqueAleatorioEnemigo() {
     let ataqueAleatorio = aleatorio(1,3)
 
-    if (ataqueAleatorio == '1'){
+    if (ataqueAleatorio == 1){
         ataqueEnemigo = 'FUEGO'
-    }else if (ataqueAleatorio == '2'){
+    }else if (ataqueAleatorio == 2){
         ataqueEnemigo = 'AGUA'
     } else {
         ataqueEnemigo = 'TIERRA'
     }
-    crearMensaje()
+    combate()
+}
+
+function combate() {
+    if (ataqueEnemigo == ataqueJugador) {
+        crearMensaje ("Empate")
+    } else if (ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA') {
+        crearMensaje ("Ganaste!!!!!")
+    } else if (ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') {
+        crearMensaje ("Ganaste!!!!! ")
+    } else if (ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA') {
+        crearMensaje ("Ganaste!!!!! ")
+    } else {
+        crearMensaje ("Perdiste")
+    }
 }
 
 
-function crearMensaje(){
+function crearMensaje(resultado) {
     let sectionMensajes = document.getElementById('mensajes')
 
     let parrafo = document.createElement('p')
-    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ', y la mascota del enemigo atacó con ' + ataqueEnemigo + ' - Pendiente'
+    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ', y la mascota del enemigo atacó con ' + ataqueEnemigo + ' -' + resultado
 
     sectionMensajes.appendChild(parrafo)
 }
 
-function aleatorio(min,max){
-    return Math.floor(Math.random()*(max-min+1)+1)
+function aleatorio(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min)
 }   
 
 window.addEventListener('load', iniciarJuego)
